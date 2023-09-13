fcb1ab1f7c9313ba58b362619c7534f3f515401c
zhangru <2495760174@qq.com>
2023-09-13 19:18:20 +0800

    实现调用远程代码沙箱

--- src/main/java/com/ink/inkOJbackend/judge/codeSandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/ink/inkOJbackend/judge/codeSandbox/impl/RemoteCodeSandbox.java
@@ -1,5 +1,10 @@
 package com.ink.inkOJbackend.judge.codeSandbox.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.ink.inkOJbackend.common.ErrorCode;
+import com.ink.inkOJbackend.exception.BusinessException;
 import com.ink.inkOJbackend.judge.codeSandbox.CodeSandbox;
 import com.ink.inkOJbackend.judge.model.ExecuteCodeRequest;
 import com.ink.inkOJbackend.judge.model.ExecuteCodeResponse;
@@ -11,6 +16,15 @@ public class RemoteCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        return null;
+        String url = "http://localhost:8102/executeCode";
+        String requestJson = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseBody = HttpUtil.createPost(url)
+                .body(requestJson)
+                .execute()
+                .body();
+        if(!StrUtil.isNotBlank(responseBody)){
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR,"调用远程代码沙箱服务错误!");
+        }
+        return JSONUtil.toBean(responseBody, ExecuteCodeResponse.class);
     }
 }
--- src/test/java/com/ink/inkOJbackend/MainApplicationTests.java
+++ src/test/java/com/ink/inkOJbackend/MainApplicationTests.java
@@ -2,9 +2,15 @@ package com.ink.inkOJbackend;
 
 import com.ink.inkOJbackend.config.WxOpenConfig;
 import javax.annotation.Resource;
+
+import com.ink.inkOJbackend.judge.codeSandbox.impl.RemoteCodeSandbox;
+import com.ink.inkOJbackend.judge.model.ExecuteCodeRequest;
+import com.ink.inkOJbackend.judge.model.ExecuteCodeResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.DigestUtils;
+
+import java.util.ArrayList;
 
 /**
  * 主类测试
@@ -30,4 +36,24 @@ class MainApplicationTests {
         System.out.println(s);
     }
 
+    @Test
+    void testRemoteCodeSandBox(){
+        ExecuteCodeRequest codeRequest = new ExecuteCodeRequest();
+        String code = "public class Solution {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        int a = Integer.parseInt(args[0]);\n" +
+                "        int b = Integer.parseInt(args[1]);\n" +
+                "        System.out.println(a+b);\n" +
+                "    }\n" +
+                "}";
+        ArrayList<String> input = new ArrayList<>();
+        input.add("1 2");
+        input.add("2 2");
+        codeRequest.setCode(code);
+        codeRequest.setInput(input);
+        codeRequest.setLanguage("java");
+        RemoteCodeSandbox remoteCodeSandbox = new RemoteCodeSandbox();
+        ExecuteCodeResponse re = remoteCodeSandbox.executeCode(codeRequest);
+        System.out.println(re);
+    }
 }
